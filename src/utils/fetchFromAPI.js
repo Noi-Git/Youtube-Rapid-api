38d7bb090448b937528a44a5e87a3245ad286d32
Noi-Git <noi.patsin@gmail.com>
2023-03-07 22:38:28 -0500

    add search api

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,10 +3,19 @@ import axios from 'axios'
 
 const axios = require('axios')
 
+// look for the url you want on the left of Rapid api
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search'
+
 const options = {
   method: 'GET',
-  url: 'https://youtube-v31.p.rapidapi.com/captions',
-  params: { part: 'snippet', videoId: 'M7FIvfx5J10' },
+  url: 'https://youtube-v31.p.rapidapi.com/search',
+  params: {
+    q: 'music',
+    part: 'snippet,id',
+    regionCode: 'US',
+    maxResults: '50',
+    order: 'date',
+  },
   headers: {
     'X-RapidAPI-Key': 'dd0c93f82cmshc808e65ae33495cp1663c1jsn19048b5bf88d',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
