485180d6516c6d803d506188ffde9a708e560a71
Noi-Git <noi.patsin@gmail.com>
2023-03-07 22:27:47 -0500

    add videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
+import { Sidebar, Videos } from '../components'
 
 const Feed = () => {
   return (
@@ -20,6 +20,18 @@ const Feed = () => {
         >
           Copyright 2022 JSM Media
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflow: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          New &nbsp;
+          <span style={{ color: '#f31503' }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   )
